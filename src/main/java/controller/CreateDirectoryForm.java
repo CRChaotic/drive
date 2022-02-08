b332afb09f3068af9103e8570b7f3b690b2fa935
CRC <crcismetm@gmail.com>
2022-02-09 00:21:34 +0800

    Directory form

---
+++ src/main/java/controller/CreateDirectoryForm.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class CreateDirectoryForm {
+    private int parentDirectoryId;
+    private String directoryName;
+
+    public int getParentDirectoryId() {
+        return parentDirectoryId;
+    }
+
+    public void setParentDirectoryId(int parentDirectoryId) {
+        this.parentDirectoryId = parentDirectoryId;
+    }
+
+    public String getDirectoryName() {
+        return directoryName;
+    }
+
+    public void setDirectoryName(String directoryName) {
+        this.directoryName = directoryName;
+    }
+}
