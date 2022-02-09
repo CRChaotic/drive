af74a2cfeea60ea05abd9a1854e89b18bf457fb1
CRC <crcismetm@gmail.com>
2022-02-09 21:05:24 +0800

    Update

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.AdminService;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+    private AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 }
