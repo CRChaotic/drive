e99f804d61666be7af84be160e2b2c1276c948ce
CRC <crcismetm@gmail.com>
2022-02-09 21:40:52 +0800

    Implementing admin controller

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,15 +1,28 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pojo.ReportedFile;
+import pojo.User;
 import service.AdminService;
+
+import javax.servlet.http.HttpSession;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-    private AdminService adminService;
+    private final AdminService adminService;
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+
+    @GetMapping("/reportedFiles")
+    public List<ReportedFile> getAllReportedFiles(HttpSession session){
+        User user = (User)session.getAttribute("user");
+        return adminService.getAllReportedFiles(user);
     }
 }
